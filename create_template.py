0d247ff3445ebbfc50770c58ede00b6d63a223de
arif599 <arifullashaik01@gmail.com>
2022-07-16 21:29:52 -0700

    adding template to create file from problem name

---
+++ create_template.py
@@ -0,0 +1,23 @@
+def create(problem):
+    problem = problem.replace(".", "")
+    file_name = problem.replace(" ", "_") + ".py"
+    file_name = "./Level 1/" + file_name
+    print(file_name)
+    with open(file_name, "w") as f:
+        f.write('"""\n')
+        f.write("Understand:\n")
+        f.write("\tProblem Statement:\n")
+        f.write("\tQuestions:\n")
+        f.write("\tTest Cases:\n")
+        f.write("\n")
+        f.write("Match:\n")
+        f.write("\n")
+        f.write("Plan:\n")
+        f.write("Optimization:")
+        f.write('"""\n')
+        f.write("\n")
+        f.write('if __name__ == "__main__":\n\tpass')
+
+if __name__ == "__main__":
+    user_input = input("Enter problem: ")
+    create(user_input)
